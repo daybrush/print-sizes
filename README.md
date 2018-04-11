e88250b4c05b135c209dfb2618539909d522bad3
Daybrush <itoolsg@gmail.com>
2018-04-12 04:12:25 +0900

    add detail

--- README.md
+++ README.md
@@ -7,16 +7,38 @@ $ npm install print-coveralls
 
 ## How to use
 ```
-print-coveralls ./coverage/lcov.info
+print-coveralls
+print-coveralls --path=./coverage/lcov.info
+print-coveralls --path=./coverage/lcov.info --detail=high,medium,low
 ```
 
+* path : coverage information's path.
+* detail : show files' coverage to the (high, medium, low)
 
 ## Output
 * Coverage is coveralls's coverage
 ```
-=============================== Coverage summary ===============================
+=============================== Coveralls summary ===============================
 Branches      : 58.1% (423 / 728)
 Lines         : 66.22% (806 / 1217)
 Coverage      : 63.18% (1229 / 1945)
+================================================================================
+/src/Animator.js                : 43.61% (99 / 227)
+/src/EventTrigger.js            : 45% (27 / 60)
+/src/Frame.js                   : 97% (162 / 167)
+/src/FrameTimeline.js           : 78.43% (40 / 51)
+/src/PropertyObject.js          : 65.38% (34 / 52)
+/src/SceneItem.js               : 76.16% (262 / 344)
+/src/Timeline.js                : 90.24% (37 / 41)
+/src/consts.js                  : 100% (15 / 15)
+/src/cubicBezier.js             : 15.78% (3 / 19)
+/src/utils.js                   : 64.44% (29 / 45)
+/src/css/Frame.js               : 93.54% (29 / 31)
+/src/css/SceneItem.js           : 46.82% (192 / 410)
+/src/css/consts.js              : 57.14% (12 / 21)
+/src/css/utils.js               : 17.94% (7 / 39)
+/src/utils/color.js             : 7.69% (5 / 65)
+/src/utils/dot.js               : 82.03% (105 / 128)
+/src/utils/property.js          : 74.34% (171 / 230)
 ================================================================================
 ```
--- bin/coveralls.js
+++ bin/coveralls.js
@@ -2,37 +2,72 @@
 
 const fs = require('fs');
 const path = require('path');
-const args = process.argv[2] || "./coverage/lcov.info";
+const [,, ...args] = process.argv;
+const info = {};
 
-const branches = {
-	now: 0,
-	all: 0,
-};
-const lines = {
-	now: 0,
-	all: 0,
-};
-const data = fs.readFileSync(path.resolve(process.cwd(), args));
-const texts = data.toString().split("\n");
+args.forEach(arg => {
+	const [name, value] = arg.split("=");
+
+	info[name.replace("--", "")] = value;
+});
+
+info.path = info.path || "./coverage/lcov.info";
+info.detail = (info.detail || "high,medium,low").split(",");
+
+const detail = info.detail;
+const dir = process.cwd();
+const data = fs.readFileSync(path.resolve(dir, info.path));
+const text = data.toString();
 
 
-lines.all = texts.filter(text => /^DA/g.test(text));
-lines.now = lines.all.filter(text => text.indexOf(",0") === -1);
-branches.all = texts.filter(text => /^BRDA/g.test(text));
-branches.now = branches.all.filter(text => !/,0$/g.test(text));
+let length = 0;
 
-lines.now = lines.now.length;
-lines.all = lines.all.length;
-branches.now = branches.now.length;
-branches.all = branches.all.length;
-const coverage = {
-	now: branches.now + lines.now,
-	all: branches.all + lines.all,
+
+
+
+
+function getCoverage(text) {
+	const branches = {
+		now: 0,
+		all: 0,
+	};
+	const lines = {
+		now: 0,
+		all: 0,
+	};
+	const texts = text.split("\n");
+
+	lines.all = texts.filter(text => /^DA/g.test(text));
+	lines.now = lines.all.filter(text => text.indexOf(",0") === -1);
+	branches.all = texts.filter(text => /^BRDA/g.test(text));
+	branches.now = branches.all.filter(text => !/,0$/g.test(text));
+	lines.now = lines.now.length;
+	lines.all = lines.all.length;
+	branches.now = branches.now.length;
+	branches.all = branches.all.length;
+	const coverage = {
+		now: branches.now + lines.now,
+		all: branches.all + lines.all,
+	}
+
+	lines.percent = parseInt(lines.now / lines.all * 10000) / 100;
+	branches.percent = parseInt(branches.now / branches.all * 10000) / 100;
+	coverage.percent = parseInt(coverage.now / coverage.all * 10000) / 100;
+
+	
+	const filename = texts[0].replace(dir, "");
+
+
+	length = Math.max(filename.length, length);
+	return {
+		lines,
+		branches,
+		coverage,
+		filename,
+	};
 }
 
-lines.percent = parseInt(lines.now / lines.all * 10000) / 100;
-branches.percent = parseInt(branches.now / branches.all * 10000) / 100;
-coverage.percent = parseInt(coverage.now / coverage.all * 10000) / 100;
+const {coverage, lines, branches} = getCoverage(text);
 
 /*
 Statements   : 66.42% ( 819/1233 )
@@ -44,4 +79,30 @@ console.log("=============================== Coveralls summary =================
 console.log("\x1b[33m\x1B[1m" + `Branches      : ${branches.percent}% (${branches.now} / ${branches.all})`);
 console.log("\x1b[33m\x1B[1m" + `Lines         : ${lines.percent}% (${lines.now} / ${lines.all})`);
 console.log("\x1b[33m\x1B[1m" + `Coverage      : ${coverage.percent}% (${coverage.now} / ${coverage.all})`);
+console.log("\x1b[0m" + "================================================================================");
+text.split("SF:").map(t => getCoverage(t)).forEach(({filename, coverage}) => {
+	if (!coverage.all) {
+		return;
+	}
+	let color = "";
+	const percent = coverage.percent;
+
+	if (percent > 80) {
+		color = "\x1b[32m";
+		if (!~detail.indexOf("high")) {
+			return;
+		}
+	} else if (percent > 60) {
+		color = "\x1b[33m";
+		if (!~detail.indexOf("medium")) {
+			return;
+		}
+	} else {
+		color = "\x1b[31m";
+		if (!~detail.indexOf("low")) {
+			return;
+		}
+	}
+	console.log("\x1b[0m\x1B[1m" + `${filename.padEnd(length + 10)}: ${color}${percent}% (${coverage.now} / ${coverage.all})`);
+});
 console.log("\x1b[0m" + "================================================================================");
